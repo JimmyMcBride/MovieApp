bc530f3fe6ec18a864cb252a802a9346e93ca942
Jimmy McBride <mcbride967@gmail.com>
2021-12-04 18:47:57 -0500

    testing out compose

--- app/src/main/java/com/example/movieapp/view/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/view/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.movieapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.movieapp.R
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MessageCard(Message("Jimmy McBride", "How To Fast"))
+        }
     }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
+}
+
+@Preview
+@Composable
+fun ComposablePreview() {
+    MessageCard(Message("Jimmy McBride", "How To Fast"))
 }
